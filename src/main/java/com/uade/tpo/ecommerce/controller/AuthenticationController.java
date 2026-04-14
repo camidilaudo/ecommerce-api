486660082cd09cc196b6127356624d5d62796fe4
Camila <camiladilaudo@icloud.com>
2026-04-14 10:32:35 -0300

    implementación de AuthenticationController con login y register

--- src/main/java/com/uade/tpo/ecommerce/controller/AuthenticationController.java
+++ src/main/java/com/uade/tpo/ecommerce/controller/AuthenticationController.java
@@ -1,4 +1,26 @@
 package com.uade.tpo.ecommerce.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import com.uade.tpo.ecommerce.dto.LoginRequest;
+import com.uade.tpo.ecommerce.dto.RegisterRequest;
+import com.uade.tpo.ecommerce.service.AuthenticationService;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest request) {
+        return authenticationService.login(request);
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestBody RegisterRequest request) {
+        return authenticationService.register(request);
+    }
 }
