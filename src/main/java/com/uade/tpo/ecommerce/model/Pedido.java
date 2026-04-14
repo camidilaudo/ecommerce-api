d14dd74e521e93f6c7e5ce283db511f4d411fea0
Camila <camila@Mac.fibertel.com.ar>
2026-04-14 10:13:44 -0300

    agregado campo stock en Producto

--- src/main/java/com/uade/tpo/ecommerce/model/Pedido.java
+++ src/main/java/com/uade/tpo/ecommerce/model/Pedido.java
@@ -3,16 +3,21 @@ package com.uade.tpo.ecommerce.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+
 @Data
 @Entity(name = "pedidos")
 public class Pedido {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String detalle;
+
+    private Double total;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "usuario_id")
-    private Usuario usuario; // Assuming Usuario is another entity class
+    private Usuario usuario;
 
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<PedidoItem> items = new ArrayList<>();
 }
