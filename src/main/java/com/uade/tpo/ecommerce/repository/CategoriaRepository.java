b1cd8f4709da7e7363982c7a2028f32ab8ce54e3
Camila <camiladilaudo@icloud.com>
2026-04-14 19:01:07 -0300

    feat(categoria): agregar busqueda por nombre

--- src/main/java/com/uade/tpo/ecommerce/repository/CategoriaRepository.java
+++ src/main/java/com/uade/tpo/ecommerce/repository/CategoriaRepository.java
@@ -1,8 +1,17 @@
 package com.uade.tpo.ecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.uade.tpo.ecommerce.model.Categoria;
 
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+import java.util.Optional;
+
+@Repository
+public interface CategoriaRepository 
+        extends JpaRepository<Categoria, Long> {
+
+    // Buscar categoria por nombre
+    Optional<Categoria> findByNombre(String nombre);
 
 }
