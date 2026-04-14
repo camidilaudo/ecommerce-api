68b12132f91bb26e422d77682ad68b75b205ddab
Camila <camiladilaudo@icloud.com>
2026-04-14 12:42:46 -0300

    feat(register): agregar @Valid en endpoint register

--- src/main/java/com/uade/tpo/ecommerce/dto/RegisterRequest.java
+++ src/main/java/com/uade/tpo/ecommerce/dto/RegisterRequest.java
@@ -5,6 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
 @Data
@@ -12,11 +17,26 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
+
+    @NotBlank(message = "El nombre de usuario es obligatorio")
     private String nombreUsuario;
+
+    @NotBlank(message = "El nombre es obligatorio")
     private String nombre;
+
+    @NotBlank(message = "El apellido es obligatorio")
     private String apellido;
+
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "Email inválido")
     private String email;
+
+    @NotBlank(message = "La contraseña es obligatoria")
     private String password;
-    private LocalDate fechaNacimiento; // Formato esperado en JSON: "YYYY-MM-DD"
+
+    @NotNull(message = "La fecha de nacimiento es obligatoria")
+    private LocalDate fechaNacimiento;
+
+    @NotNull(message = "El sexo es obligatorio")
     private Sexo sexo;
 }
