d18fc0a45addf535b88d4cdc6fde6d5f8794ce9d
Joko <joaquinfernandes00@gmail.com>
2026-05-08 21:51:14 -0300

    added frontend

---
+++ frontend/src/components/ProductList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import products from '../data/products.json';
+import CardProductos from './CardProductos';
+import './ProductList.css';
+
+const ProductList = () => {
+  return (
+    <div className="product-list-container">
+      <h2 className="product-list-title">Nuestros Productos</h2>
+      <div className="products-grid">
+        {products.map((product) => (
+          
+          <CardProductos key={product.id} product={product} >
+            <p>{product.gratis ? 'Gratis' : ''}</p>
+            <p>{product.internacional ? 'Envio Internacional' : ''}</p>
+          </CardProductos>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
